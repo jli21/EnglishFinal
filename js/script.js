68c7384f58b1135bf3d320f9382cda29758e812e
penguinbearking <snarendran@ucvts.org>
2018-10-30 16:50:17 -0400

    finished head

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-//Stuff
+function wsizepopup() {
+	alert("For best results, it is not recomended to size the width of this page to less than 1200 pixels")
+	var pathname = window.location.pathname;
+	if (pathname == "../index.html") {
+		document.getElementById("TOC0").style.font-style = "italic"; 
+		alert(pathname)
+	}
+
+}
+
+function TOCcolor() {
+
+}
