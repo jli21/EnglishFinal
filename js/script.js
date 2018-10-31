5e72088dbdc9482c4e5991d977c02dbc00dd2033
penguinbearking <snarendran@ucvts.org>
2018-10-30 21:41:31 -0400

    added js stuff and home.html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,85 @@
-function wsizepopup() {
-	alert("For best results, it is not recomended to size the width of this page to less than 1200 pixels")
-	var pathname = window.location.pathname;
-	if (pathname == "../index.html") {
-		document.getElementById("TOC0").style.font-style = "italic"; 
-		alert(pathname)
+function startup() {
+
+	if (localStorage.getItem("visited") != "true") {
+		alert("For best results, it is not recomended to size the width of this page to less than 1200 pixels")
+		localStorage.setItem("visited", "true");
 	}
+
+	/*
+	var pathname = window.location.pathname;
+	//alert(pathname)
+
+	var tf = (pathname.endsWith("index.html"))
+	if (tf == true) {
+		document.getElementById("TOC0").style.fontSize="22px";
+		document.getElementById("TOC0").style.fontWeight="bold";
+	}
+
+	var tf = (pathname.endsWith("timeline.html"))
+	if (tf == true) {
+		document.getElementById("TOC1").style.fontSize="22px";
+		document.getElementById("TOC1").style.fontWeight="bold";
+	}
+
+	var tf = (pathname.endsWith("conclusion.html"))
+	if (tf == true) {
+		document.getElementById("TOC2").style.fontSize="22px";
+		document.getElementById("TOC2").style.fontWeight="bold";
+	}
+
+	var tf = (pathname.endsWith("sources.html"))
+	if (tf == true) {
+		document.getElementById("TOC3").style.fontSize="22px";
+		document.getElementById("TOC3").style.fontWeight="bold";
+	}
+	*/
 
 }
 
-function TOCcolor() {
+function TOC0() {
+	document.getElementById("TOC0").style.fontSize="22px";
+	document.getElementById("TOC0").style.fontWeight="bold";
+	document.getElementById("TOC1").style.fontSize="18px";
+	document.getElementById("TOC1").style.fontWeight="normal";
+	document.getElementById("TOC2").style.fontSize="18px";
+	document.getElementById("TOC2").style.fontWeight="normal";
+	document.getElementById("TOC3").style.fontSize="18px";
+	document.getElementById("TOC3").style.fontWeight="normal";
+
+}
+
+function TOC1() {
+	document.getElementById("TOC0").style.fontSize="18px";
+	document.getElementById("TOC0").style.fontWeight="normal";
+	document.getElementById("TOC1").style.fontSize="22px";
+	document.getElementById("TOC1").style.fontWeight="bold";
+	document.getElementById("TOC2").style.fontSize="18px";
+	document.getElementById("TOC2").style.fontWeight="normal";
+	document.getElementById("TOC3").style.fontSize="18px";
+	document.getElementById("TOC3").style.fontWeight="normal";
+
+}
+
+function TOC2() {
+	document.getElementById("TOC0").style.fontSize="18px";
+	document.getElementById("TOC0").style.fontWeight="normal";
+	document.getElementById("TOC1").style.fontSize="18px";
+	document.getElementById("TOC1").style.fontWeight="normal";
+	document.getElementById("TOC2").style.fontSize="22px";
+	document.getElementById("TOC2").style.fontWeight="bold";
+	document.getElementById("TOC3").style.fontSize="18px";
+	document.getElementById("TOC3").style.fontWeight="normal";
+
+}
+
+function TOC3() {
+	document.getElementById("TOC0").style.fontSize="18px";
+	document.getElementById("TOC0").style.fontWeight="normal";
+	document.getElementById("TOC1").style.fontSize="18px";
+	document.getElementById("TOC1").style.fontWeight="normal";
+	document.getElementById("TOC2").style.fontSize="18px";
+	document.getElementById("TOC2").style.fontWeight="normal";
+	document.getElementById("TOC3").style.fontSize="22px";
+	document.getElementById("TOC3").style.fontWeight="bold";
 
 }
